061aa9cb435bf8f28b434fee85633439204476ee
kaiyuan-guan <129572528+kaiyuan-guan@users.noreply.github.com>
2023-04-01 12:54:48 +0800

    Update jiekou.js

---
+++ jiekou.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const PORT=process.env.PORT||3000;
+const exampleData=[
+    {
+        id:1,
+        name:"Zhang",
+        age:20
+    },
+    {
+        id:2,
+        name:"Guan",
+        age:21
+    }
+];
+app.length('/api/data',(req,res) =>{
+    res.json(exampleData);
+});
+app.listen(PORT,() =>{
+    console.log('Sever running on port ${PORT}');
+});
